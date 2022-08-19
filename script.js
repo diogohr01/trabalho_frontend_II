e49022ed5cf47e9c38cbef271cd71426b053efba
diogohr01 <diogorodrigues699@yahoo.com>
2022-08-19 18:23:26 -0300

    nao terminei ainda

---
+++ script.js
@@ -0,0 +1,77 @@
+let playerWin = 0
+let machineWin = 0
+let empate = 0
+
+const round = document.getElementById('enviar')
+const pedra = document.getElementById('opçãoPedra');
+const papel = document.getElementById('opçãoPapel');
+const tesoura = document.getElementById('opçãoTesoura');
+ const valorcomputador = document.getElementById('pontosComputador')
+const valorusuario = document.getElementById('pontosUsuário')
+
+
+function game(escolhaJogador, escolhacomputador) {
+    escolhacomputador = parseInt(Math.random() * 3 + 1);
+    if (escolhaJogador == "opçãoPedra") {
+        escolhaJogador = 1;
+    } else if (escolhaJogador == "opçãoPapel") {
+        escolhaJogador = 2;
+    } else if (escolhaJogador == "opçãoTesoura") {
+        escolhaJogador = 3;
+    };
+
+    if (escolhaJogador = 1 && escolhacomputador == 3) {
+        playerWin = playerWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
+    } else if (escolhaJogador = 1 && escolhacomputador == 2) {
+        machineWin = machineWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
+    } else if (escolhaJogador = 1 && escolhacomputador == 1) {
+        empate++;
+        document.querySelector('.resultado > p').innerText = 'Empatou!';
+    }
+
+    if (escolhaJogador = 2 && escolhacomputador == 1) {
+        playerWin = playerWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
+    } else if (escolhaJogador = 2 && escolhacomputador == 3) {
+        machineWin = machineWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
+    } else if (escolhaJogador = 2 && escolhacomputador == 2) {
+        empate++;
+        document.querySelector('.resultado > p').innerText = 'Empatou!';
+
+    }
+
+    if (escolhaJogador = 3 && escolhacomputador == 2) {
+        playerWin = playerWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
+    } else if (escolhaJogador = 3 && escolhacomputador == 1) {
+        machineWin = machineWin + 1;
+        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
+    } else if (escolhaJogador = 3 && escolhacomputador == 3) {
+        empate++;
+        document.querySelector('.resultado > p').innerText = 'Empatou!';
+
+    }
+}
+
+function main (){
+    pedra.addEventListener('click',function() {
+        game("opçãoPedra");
+    })
+
+    papel.addEventListener('click',function() {
+        game("opçãoPapel")
+    })
+
+    tesoura.addEventListener('click',function() {
+        game("opçãoTesoura")
+    })
+}
+
+main();
+valorcomputador.innerHTML = machineWin;
+valorusuario.innerHTML = playerWin;
+
+ 
