efb209d2e475b79f520039925612ef14e12b374b
diogohr01 <diogorodrigues699@yahoo.com>
2022-08-22 15:40:58 -0300

    pronto

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let playerWin = 0
-let machineWin = 0
-let empate = 0
-
 const round = document.getElementById('enviar')
 const pedra = document.getElementById('opçãoPedra');
 const papel = document.getElementById('opçãoPapel');
 const tesoura = document.getElementById('opçãoTesoura');
- const valorcomputador = document.getElementById('pontosComputador')
-const valorusuario = document.getElementById('pontosUsuário')
+const valorcomputador = document.querySelector('.pontuacao > #pontosComputador')
+const valorusuario = document.querySelector('.pontuacao > #pontosUsuario')
+
+let playerWin = 0
+let machineWin = 0
+let empate = 0
+
 
 
 function game(escolhaJogador, escolhacomputador) {
@@ -20,41 +21,46 @@ function game(escolhaJogador, escolhacomputador) {
         escolhaJogador = 3;
     };
 
-    if (escolhaJogador = 1 && escolhacomputador == 3) {
+    if (escolhaJogador == 1 && escolhacomputador == 3) {
         playerWin = playerWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
-    } else if (escolhaJogador = 1 && escolhacomputador == 2) {
+        document.querySelector('.resultado > p').innerText = 'Você ganhou! O PC escolheu pedra.';
+    } else if (escolhaJogador == 1 && escolhacomputador == 2) {
         machineWin = machineWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
-    } else if (escolhaJogador = 1 && escolhacomputador == 1) {
+        document.querySelector('.resultado > p').innerText = 'Você perdeu! O PC escolheu papel.';
+    } else if (escolhaJogador == 1 && escolhacomputador == 1) {
         empate++;
-        document.querySelector('.resultado > p').innerText = 'Empatou!';
+        document.querySelector('.resultado > p').innerText = 'Empatou! Os dois escolheram pedra.';
     }
 
-    if (escolhaJogador = 2 && escolhacomputador == 1) {
+    if (escolhaJogador == 2 && escolhacomputador == 1) {
         playerWin = playerWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
-    } else if (escolhaJogador = 2 && escolhacomputador == 3) {
+        document.querySelector('.resultado > p').innerText = 'Você ganhou! O PC escolheu pedra.';
+    } else if (escolhaJogador == 2 && escolhacomputador == 3) {
         machineWin = machineWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
-    } else if (escolhaJogador = 2 && escolhacomputador == 2) {
+        document.querySelector('.resultado > p').innerText = 'Você perdeu! O PC escolheu tesoura.';
+    } else if (escolhaJogador == 2 && escolhacomputador == 2) {
         empate++;
-        document.querySelector('.resultado > p').innerText = 'Empatou!';
+        document.querySelector('.resultado > p').innerText = 'Empatou! Os dois escolheram papel.';
 
     }
 
-    if (escolhaJogador = 3 && escolhacomputador == 2) {
+    if (escolhaJogador == 3 && escolhacomputador == 2) {
         playerWin = playerWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você ganhou!';
-    } else if (escolhaJogador = 3 && escolhacomputador == 1) {
+        document.querySelector('.resultado > p').innerText = 'Você ganhou! O PC escolheu papel.';
+    } else if (escolhaJogador == 3 && escolhacomputador == 1) {
         machineWin = machineWin + 1;
-        document.querySelector('.resultado > p').innerText = 'Você perdeu!';
-    } else if (escolhaJogador = 3 && escolhacomputador == 3) {
+        document.querySelector('.resultado > p').innerText = 'Você perdeu! O PC escolheu pedra';
+    } else if (escolhaJogador == 3 && escolhacomputador == 3) {
         empate++;
-        document.querySelector('.resultado > p').innerText = 'Empatou!';
+        document.querySelector('.resultado > p').innerText = 'Empatou! Os dois escolheram tesoura.';
 
     }
+
+     valorcomputador.innerText = machineWin;
+     valorusuario.innerText = playerWin;
+   
 }
+game()
 
 function main (){
     pedra.addEventListener('click',function() {
@@ -71,7 +77,8 @@ function main (){
 }
 
 main();
-valorcomputador.innerHTML = machineWin;
-valorusuario.innerHTML = playerWin;
 
- 
+function limpar(){
+    document.location.reload(true);
+}
+
